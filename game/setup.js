154340fb980b240a3303cd9409f5e31ffc0df776
Vins MacBook <vinavarghese@gmail.com>
2020-10-01 11:44:21 -0700

    Balancing setup/game stats

--- game/setup.js
+++ game/setup.js
@@ -4,15 +4,15 @@ const Weapon = require("../classes/Weapon");
 // ADD/EDIT WEAPONS
 // Instances of Weapon
 const Sai = new Weapon("Sai", 18, 7);
-const Nunchaku = new Weapon("Nunchaku", 15, 8);
+const Nunchaku = new Weapon("Nunchaku", 16, 8);
 const Katanas = new Weapon("Twin Katana", 20, 5);
 const Bo = new Weapon("Bo", 17, 5);
 
 // ADD/EDIT CHARACTERS & EQUIP WEAPON
 // Instances of Character
 const Raphael = new Character("Raphael", 115, Sai);
-const Michelangelo = new Character("Michelangelo", 90, Nunchaku);
-const Leonardo = new Character("Leonardo", 125, Katanas);
+const Michelangelo = new Character("Michelangelo", 110, Nunchaku);
+const Leonardo = new Character("Leonardo", 115, Katanas);
 const Donatello = new Character("Donatello", 110, Bo);
 
 // DEPLOY CHARACTERS 
